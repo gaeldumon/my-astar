3a63b2c06d7aa9aa3cf13951982eabce28059035
= <slotuser@gmail.com>
2021-03-06 22:57:01 +0100

    Avancement tweaks (inputs, refacto colors..) + add css.

--- main.js
+++ main.js
@@ -4,16 +4,33 @@ const canvas = document.getElementById('pathscreen')
 const ctx = canvas.getContext('2d')
 if (!ctx) alert('Canvas not supported')
 
-// Radius of each cell of the grid
+const startNodeInput = document.getElementById('startNode')
+const goalNodeInput = document.getElementById('goalNode')
+const launchBtn = document.getElementById('launchBtn')
+
+const COLORS = {
+	BLACK: 'rgb(0,0,0)',
+	GREEN: 'rgb(88,239,98)',
+	PINK: 'rgb(252,106,240)',
+	YELLOW: 'rgb(252,248,45)',
+	LIGHT_BLUE: 'rgb(7,240,252)',
+	BLUE: 'rgb(73,73,252)',
+	GREY: 'rgb(163, 163, 163)',
+	TRANSPARENT: 'rgb(255,255,255)'
+}
+const START_COLOR = COLORS.BLUE
+const GOAL_COLOR = COLORS.GREEN
+const OBSTACLE_COLOR = COLORS.BLACK
+const CLOSED_SET_COLOR = COLORS.LIGHT_BLUE
+const OPEN_SET_COLOR = COLORS.PINK
+const PATH_COLOR = COLORS.YELLOW
+const EMPTY_NODE_COLOR = COLORS.GREY
+
 const CELL_RADIUS = 6
-// Execution speed in seconds
 const SPEED = 0.08
-// Number of obstacles to avoid
 const OBSTACLES = 150
-// Between 0 and 2499
-const START_INDEX = 0
-// Between 0 and 2499
-const GOAL_INDEX = 2499
+const START_INDEX = startNodeInput.value
+const GOAL_INDEX = goalNodeInput.value
 
 /**
  * Node constructor
@@ -31,49 +48,20 @@ function Node(x, y, i) {
 	this.previous = 0
 	this.index = i
 	this.isObstacle = false
-
 	this.radius = CELL_RADIUS
-	this.color = 'rgb(163, 163, 163)'
-	this.colorFilled = 'rgb(0, 0, 0)'
-	this.colorFilledArrival = 'rgb(88, 239, 98)'
-	this.colorFilledStart = 'rgb(73, 73, 252)'
-	this.colorFilledNeighbor = 'rgb(252, 106, 240)'
-	this.colorFilledClosedSet = 'rgb(7, 240, 252)'
-	this.colorFilledOpenSet = 'rgb(252, 248, 45)'
-	this.colorFilledPath = 'rgb(252, 248, 45)'
 
-	this.draw = function () {
-		ctx.strokeStyle = this.color
-		ctx.beginPath()
-		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true)
-		ctx.stroke()
-	}
-
-	this.drawFilled = function (type) {
-		if (type === 'start') {
-			ctx.fillStyle = this.colorFilledStart
-		} else if (type === 'goal') {
-			ctx.fillStyle = this.colorFilledArrival
-		} else if (type === 'neighbor') {
-			if (this.isObstacle) {
-				ctx.fillStyle = 'rgba(0, 0, 0, 0)'
-			} else {
-				ctx.fillStyle = this.colorFilledNeighbor
-			}
-		} else if (type === 'closedSet') {
-			ctx.fillStyle = this.colorFilledClosedSet
-		} else if (type === 'openSet') {
-			ctx.fillStyle = this.colorFilledOpenSet
-		} else if (type === 'obstacle') {
-			ctx.fillStyle = this.colorFilled
-		} else if (type === 'path') {
-			ctx.fillStyle = this.colorFilledPath
+	this.draw = function (color, status = null) {
+		if (status === 'empty') {
+			ctx.strokeStyle = color
+			ctx.beginPath()
+			ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true)
+			ctx.stroke()
 		} else {
-			ctx.fillStyle = this.colorFilled
+			ctx.fillStyle = color
+			ctx.beginPath()
+			ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true)
+			ctx.fill()
 		}
-		ctx.beginPath()
-		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true)
-		ctx.fill()
 	}
 }
 
@@ -89,9 +77,9 @@ function Grid() {
 		for (let x = this.cellRadius; x <= canvas.width; x += this.cellRadius * 2) {
 			for (let y = this.cellRadius; y <= canvas.height; y += this.cellRadius * 2) {
 				const node = new Node(x, y, this.countNode, this)
-				this.countNode += 1
-				node.draw()
+				node.draw(EMPTY_NODE_COLOR, 'empty')
 				this.nodes.push(node)
+				this.countNode += 1
 			}
 		}
 	}
@@ -103,19 +91,19 @@ function Grid() {
  * @param {Node} node 
  */
 function getNeighbors(grid, node) {
-	const saut = CELL_RADIUS * 2
+	const offset = CELL_RADIUS * 2
 	const x = node.x
 	const y = node.y
 	const neighbors = []
 
-	const upIndex = grid.nodes.findIndex((el) => el.x === x && el.y === y - saut)
-	const downIndex = grid.nodes.findIndex((el) => el.x === x && el.y === y + saut)
-	const rightIndex = grid.nodes.findIndex((el) => el.x === x + saut && el.y === y)
-	const leftIndex = grid.nodes.findIndex((el) => el.x === x - saut && el.y === y)
-	const diagDownRight = grid.nodes.findIndex((el) => el.x === x + saut && el.y === y + saut)
-	const diagDownLeft = grid.nodes.findIndex((el) => el.x === x - saut && el.y === y + saut)
-	const diagUpRight = grid.nodes.findIndex((el) => el.x === x + saut && el.y === y - saut)
-	const diagUpLeft = grid.nodes.findIndex((el) => el.x === x - saut && el.y === y - saut)
+	const upIndex = grid.nodes.findIndex((el) => el.x === x && el.y === y - offset)
+	const downIndex = grid.nodes.findIndex((el) => el.x === x && el.y === y + offset)
+	const rightIndex = grid.nodes.findIndex((el) => el.x === x + offset && el.y === y)
+	const leftIndex = grid.nodes.findIndex((el) => el.x === x - offset && el.y === y)
+	const diagDownRight = grid.nodes.findIndex((el) => el.x === x + offset && el.y === y + offset)
+	const diagDownLeft = grid.nodes.findIndex((el) => el.x === x - offset && el.y === y + offset)
+	const diagUpRight = grid.nodes.findIndex((el) => el.x === x + offset && el.y === y - offset)
+	const diagUpLeft = grid.nodes.findIndex((el) => el.x === x - offset && el.y === y - offset)
 
 	if (grid.nodes[upIndex]) neighbors.push(grid.nodes[upIndex])
 	if (grid.nodes[downIndex]) neighbors.push(grid.nodes[downIndex])
@@ -125,8 +113,6 @@ function getNeighbors(grid, node) {
 	if (grid.nodes[diagDownLeft]) neighbors.push(grid.nodes[diagDownLeft])
 	if (grid.nodes[diagUpRight]) neighbors.push(grid.nodes[diagUpRight])
 	if (grid.nodes[diagUpLeft]) neighbors.push(grid.nodes[diagUpLeft])
-
-	for (let n of neighbors) n.drawFilled('neighbor')
 
 	return neighbors
 }
@@ -139,9 +125,9 @@ function getNeighbors(grid, node) {
 function makeObstacles(n, grid) {
 	for (let i = 0; i <= n; i++) {
 		let randIndex = Math.floor(Math.random() * (2499 - 0) + 0)
-		let randObstacle = grid.nodes[randIndex]
-		randObstacle.isObstacle = true
-		randObstacle.drawFilled('obstacle')
+		let obstacle = grid.nodes[randIndex]
+		obstacle.isObstacle = true
+		obstacle.draw(OBSTACLE_COLOR)
 	}
 }
 
@@ -184,13 +170,17 @@ function AStar(grid, start, goal) {
 
 	// Storing the setInterval to call clearInterval later
 	let inter = setInterval(() => {
+
 		if (openSet.length > 0) {
+
 			let winner = 0
+
 			for (let i = 0; i < openSet.length; i++) {
 				if (openSet[i].f < openSet[winner].f) {
 					winner = i
 				}
 			}
+
 			let current = openSet[winner]
 
 			// Arrived at destination
@@ -206,8 +196,11 @@ function AStar(grid, start, goal) {
 			closedSet.push(current)
 
 			let neighbors = getNeighbors(grid, current)
+
 			for (const neighbor of neighbors) {
+
 				if (!closedSet.includes(neighbor) && !neighbor.isObstacle) {
+
 					let tempG = current.g + heuristic(neighbor, current)
 					let newPath = false
 
@@ -233,12 +226,14 @@ function AStar(grid, start, goal) {
 			path = []
 			let temp = current
 			path.push(temp)
+
 			while (temp.previous) {
 				path.push(temp.previous)
 				temp = temp.previous
 			}
+
 			for (let p of path) {
-				grid.nodes[p.index].drawFilled('path')
+				grid.nodes[p.index].draw(PATH_COLOR)
 			}
 			//endregion
 
@@ -247,12 +242,12 @@ function AStar(grid, start, goal) {
 			return 0
 		}
 
-		//region Drawing closedSet nodes
 		for (let n of closedSet) {
-			if (!path.includes(n))
-				grid.nodes[n.index].drawFilled('closedSet')
+			if (!path.includes(n)) grid.nodes[n.index].draw(CLOSED_SET_COLOR)
 		}
-		//endregion
+		for (let n of openSet) {
+			if (!path.includes(n)) grid.nodes[n.index].draw(OPEN_SET_COLOR)
+		}
 
 	}, SPEED * 1000)
 }
@@ -261,15 +256,32 @@ function AStar(grid, start, goal) {
 const grid = new Grid()
 grid.create()
 
-const start = grid.nodes[START_INDEX]
-const goal = grid.nodes[GOAL_INDEX]
+
+let start = grid.nodes[START_INDEX]
+let goal = grid.nodes[GOAL_INDEX]
+
+
+start.draw(START_COLOR)
+goal.draw(GOAL_COLOR)
+
+
+startNodeInput.addEventListener('change', (e) => {
+	start.draw(COLORS.TRANSPARENT)
+	start = grid.nodes[e.target.value]
+	start.draw(START_COLOR)
+})
+
+
+goalNodeInput.addEventListener('change', (e) => {
+	goal.draw(COLORS.TRANSPARENT)
+	goal = grid.nodes[e.target.value]
+	goal.draw(GOAL_COLOR)
+})
+
 
 makeObstacles(OBSTACLES, grid)
 
-start.drawFilled('start')
-goal.drawFilled('goal')
 
-const launchBtn = document.getElementById('launchBtn')
 launchBtn.addEventListener('click', (e) => {
 	AStar(grid, start, goal)
 	e.target.disabled = true
